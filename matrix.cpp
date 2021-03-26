713bb5731101dbb5def3e26d6f19f273368a1e14
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2021-03-27 00:20:27 +0200

    Create matrix.cpp

---
+++ matrix.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int A[3][2],B[3][2],C[3][2];
+void intialise_matrix(int A[],int r,int c)
+{
+  int A[3][2],B[3][2];
+for(int i=0;i<3;i++)
+{
+  for(int j=0;j<2;j++)
+  {
+ A[i][j]=0;   
+  }
+}
+  return 
+}
+int main()
+{
+int r=3,c=2;
+int A[3][2],B[3][2];
+for(int i=0;i<3;i++)
+{
+  for(int j=0;j<2;j++)
+  {
+ A[i][j]=0;   
+  }
+}
+
+
+return 0;
+}
