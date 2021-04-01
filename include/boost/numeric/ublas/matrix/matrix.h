b4420edfc53273ad5251a0c2ce4cd9a9e23313f2
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2021-04-01 13:28:11 +0200

    Update matrix.h

--- include/boost/numeric/ublas/matrix/matrix.h
+++ include/boost/numeric/ublas/matrix/matrix.h
@@ -1,13 +1,46 @@
 using namespace std;
-//templete<class T>
-class matrix{
+templete<class T>
+typedef unsigned long long ll;
+class Matrix{
+  
   public:
- void zeros(int r,int c)
-  {
-    int 
-  }
+  
+  //pointer to a series of pointer 
+  
+  ll **matrix;
+  
+  Matrix();//constructor
+  ~Matrix(); //deconstructor
+  Matrix(ll r,ll c);
+  
+  //setsize method-- reisize the matrix 
+  
+  ll **setSize(ll r,ll c);
+  
+  //getMatrix setting
+  
+  ll *getMatrix();
+  //getRow method -- will return the pointer of the begin of the array[x,0]
+  ll *getRow(ll r);
+  // getColumn method -- will return the pointer of the begin of the array[0,y]
+  ll *getColumn(ll c);
+  //destructor method -- will deallocate the memory used by matrix 
+  
+/*void zeros(unsigned r,unsigned c)
+{
+   unsigned matrix_[r+1][c+1];
+   for(unsigned i=1;i<=r;i++)
+   {
+     for(unsigned i=1;i<=r;i++)
+     {
+     matrix_[i][j]=0;
+     }
+   }
+}*/
+  
   
   private:
+  ll r,c;
   
   
 };
